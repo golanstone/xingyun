d6a15cebf00c1a059b28c3b1bf835dc754fd1868
zhimin.he <zhimin.he@enai.im>
2015-10-26 17:44:29 +0800

    add memcachestore

--- session_memory_store.go
+++ session_memory_store.go
@@ -1,5 +1,11 @@
 package xingyun
 
+import (
+	"time"
+
+	"github.com/xiaoenai/gomemcache/memcache"
+)
+
 // Simple session storage using memory, handy for development
 // **NEVER** use it in production!!!
 type memoryStore struct {
@@ -23,4 +29,42 @@ func (ms *memoryStore) GetSession(sessionID string, key string) []byte {
 
 func (ms *memoryStore) ClearSession(sessionID string, key string) {
 	delete(ms.data, sessionID+key)
+}
+
+type memcacheStore struct {
+	mc     *memcache.Client
+	logger Logger
+}
+
+func NewMemcacheStore(addr string, logger Logger) *memcacheStore {
+	return &memcacheStore{
+		mc:     memcache.New(addr),
+		logger: logger,
+	}
+}
+
+func (ms *memcacheStore) SetSession(sessionID string, key string, data []byte) {
+	_7days := 7 * 24 * int32(time.Hour.Seconds())
+	err := ms.mc.Set(&memcache.Item{Key: sessionID + ":" + key, Value: data, Expiration: _7days})
+	if err != nil {
+		ms.logger.Errorf("SetSession %s", err)
+	}
+}
+
+func (ms *memcacheStore) GetSession(sessionID string, key string) []byte {
+	item, err := ms.mc.Get(sessionID + ":" + key)
+	if err != nil {
+		if err != memcache.ErrCacheMiss {
+			ms.logger.Errorf("GetSession %s", err)
+		}
+		return nil
+	}
+	return item.Value
+}
+
+func (ms *memcacheStore) ClearSession(sessionID string, key string) {
+	err := ms.mc.Delete(sessionID + ":" + key)
+	if err != nil {
+		ms.logger.Errorf("ClearSession %s", err)
+	}
 }
